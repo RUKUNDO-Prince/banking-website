24e424d10f308d741083d09db864debc13a5132e
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-29 08:11:33 +0300

    A react banking website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1 style={{ color: 'white' }}>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
