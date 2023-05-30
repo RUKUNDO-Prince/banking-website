40c78cc74296b8f99436c3bb85ad7e719147b76e
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-30 09:26:16 +0300

    a react banking website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,14 @@ const Hero = () =>
             <span className='text-white'>20% </span> Discount For {' '}
             <span className='text-white'> 1 Month </span> Account           
           </p>
-          <div className='flex flex-row '></div>
+        </div>
+        <div className='flex flex-row justify-between items-center w-full'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>The Next <br className='sm:block hidden' /> {' '} 
+          <span className='text-gradient'>Generation</span> {' '} 
+          Payment Method</h1>
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+          </div>
         </div>
       </div>
     </section>
