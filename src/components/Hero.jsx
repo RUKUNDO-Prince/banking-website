d73f278179b4d21a379b260dafef34df84b7485c
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-30 07:52:17 +0300

    a react banking website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
-const Hero = () => {
-  return (
-    <div>
-      Hero
-    </div>
+const Hero = () => 
+ (
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className="flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6"></div>
+    </section>
   )
-}
 
 export default Hero
