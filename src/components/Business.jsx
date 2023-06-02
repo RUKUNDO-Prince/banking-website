6713abde57a9e20bf6f59d291854cb735929f78c
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-06-02 13:20:17 +0300

    a react banking site

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1} ? 'mb-6' : 'mb-0'`}>
+    <div>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain' />
+    </div>
+  </div>
+)
 
 const Business = () => {
   return (
-    <div>
-      Business
-    </div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we’ll handle the money.</h2> 
+        <p className={`${styles.paragraph}`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <Button styles='mt-10' />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
   )
 }
 
