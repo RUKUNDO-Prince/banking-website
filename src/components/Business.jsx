97952fd796513ed49a12a9c9e3e5a3d2529b4ed1
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-06-02 13:43:25 +0300

    a react banking site

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -4,9 +4,13 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1} ? 'mb-6' : 'mb-0'`}>
-    <div>
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain' />
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>{title}</h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>{content}</p>
     </div>
   </div>
 )
